33d477b34674252a3e78fb81cd42c250380c9a39
Uraneptus <uraneptus33@gmail.com>
2024-08-26 20:05:33 +0200

    bread uses fish tag

--- src/main/java/com/uraneptus/fishermens_trap/common/blocks/FishtrapBlockEntity.java
+++ src/main/java/com/uraneptus/fishermens_trap/common/blocks/FishtrapBlockEntity.java
@@ -10,6 +10,7 @@ import com.uraneptus.fishermens_trap.core.other.FTItemTags;
 import com.uraneptus.fishermens_trap.core.registry.FTBlockEntityType;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Connection;
 import net.minecraft.network.chat.Component;
@@ -127,6 +128,13 @@ public class FishtrapBlockEntity extends BlockEntity implements MenuProvider, Na
                         loottable = pLevel.getServer().getLootData().getLootTable(BuiltInLootTables.FISHING_JUNK);
                     }
                     List<ItemStack> list = loottable.getRandomItems(lootparams);
+                    //Prevent Fd slices from being collected by trap
+                    list.forEach(item -> {
+                        String name = ForgeRegistries.ITEMS.getKey(item.getItem()).getPath();
+                        if (name.contains("_slice")) {
+                            list.remove(item);
+                        }
+                    });
                     pBlockEntity.handler.handleItemsInsertion(list, itemInBaitSlot, random);
                 }
             } else {
